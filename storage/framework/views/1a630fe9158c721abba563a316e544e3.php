e77817599ced07b5aed8e95b0a46253e92455410
NIHALOUKAL <134002217+NIHALOUKAL@users.noreply.github.com>
2023-06-21 08:19:56 +0300

    Add files via upload

---
+++ storage/framework/views/1a630fe9158c721abba563a316e544e3.php
@@ -0,0 +1,10 @@
+<link href="<?php echo e(asset('front')); ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo e(asset('front')); ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo e(asset('front')); ?>/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?php echo e(asset('front')); ?>/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?php echo e(asset('front')); ?>/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo e(asset('front')); ?>/assets/css/main.css" rel="stylesheet">
+
+  <?php echo $__env->yieldPushContent('css'); ?><?php /**PATH C:\xampp\htdocs\laravel1\resources\views/front/partials/style.blade.php ENDPATH**/ ?>
